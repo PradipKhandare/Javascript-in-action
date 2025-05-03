409089b84f967a434ce55ecf0e6340447f437574
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2025-05-03 11:44:26 +0530

    Handle error in areaOfRectangle if value is negative

--- area-of-Rectangle.js
+++ area-of-Rectangle.js
@@ -1,6 +1,13 @@
 function areaOfRectangle(width, length) {
-    const area = width * length;
-    console.log(`Area of reactangle with length ${length} and width ${width} is : ${area}`);
+    if (width <= 0 || length <= 0) {
+        console.log("Wrong width or length");
+        throw new Error("Length should be a positive number");
+    } else {
+        const area = width * length;
+        console.log(`Area of reactangle with length ${length} and width ${width} is : ${area}`);
+    }
+
 }
 
 areaOfRectangle(10, 30);
+//areaOfRectangle(-10, 30);
