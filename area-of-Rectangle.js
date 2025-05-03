d933fc008b2371aeb0048eca8a74a873a0846c63
PradipKhandare <pradip.khandare@neutrinotechlabs.com>
2025-05-03 11:40:36 +0530

    Initial commit

---
+++ area-of-Rectangle.js
@@ -0,0 +1,6 @@
+function areaOfRectangle(width, length) {
+    const area = width * length;
+    console.log(`Area of reactangle with length ${length} and width ${width} is : ${area}`);
+}
+
+areaOfRectangle(10, 30);
